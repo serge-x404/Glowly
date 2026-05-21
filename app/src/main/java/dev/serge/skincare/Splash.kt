0ca42d8ceda6c8e6c3f007d40d99f4aca0f34cd2
serge <kpdev1124@gmail.com>
2026-05-21 12:31:37 +0530

    Add:Splash activity working

---
+++ app/src/main/java/dev/serge/skincare/Splash.kt
@@ -0,0 +1,23 @@
+package dev.serge.skincare
+
+import android.os.Bundle
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+import dev.serge.skincare.databinding.ActivitySplashBinding
+
+class Splash : AppCompatActivity() {
+    private lateinit var binding: ActivitySplashBinding
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        binding = ActivitySplashBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
+            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
+            insets
+        }
+    }
+}
