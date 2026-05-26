a710166f97ab379c3c8b6913d19a66d3464d7399
serge <kpdev1124@gmail.com>
2026-05-26 16:17:39 +0530

    Account setup screens(operational)

---
+++ app/src/main/java/dev/serge/skincare/account_setup/AccountSetup.kt
@@ -0,0 +1,25 @@
+package dev.serge.skincare.account_setup
+
+import android.os.Bundle
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+import dev.serge.skincare.R
+import dev.serge.skincare.databinding.ActivityAccountSetupBinding
+import dev.serge.skincare.databinding.FragmentChangePasswordBinding
+
+class AccountSetup : AppCompatActivity() {
+    private lateinit var binding: ActivityAccountSetupBinding
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        binding = ActivityAccountSetupBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
+            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
+            insets
+        }
+    }
+}
